e1af9591a1f6cec71585993d9b816a9051e6b6c0
Lars George <lars@larsgeorge.com>
2011-01-03 15:09:15 +0100

    Adds callouts.

--- ch04/src/main/java/PutExample.java
+++ ch04/src/main/java/PutExample.java
@@ -1,4 +1,4 @@
-// cc PutExample Example application to insert data into HBase
+// cc PutExample Example application inserting data into HBase
 // vv PutExample
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -11,11 +11,16 @@ import java.io.IOException;
 public class PutExample {
 
   public static void main(String[] args) throws IOException {
-    Configuration conf = HBaseConfiguration.create();
-    HTable table = new HTable(conf, "testtable");
-    Put put = new Put(Bytes.toBytes("row1"));
-    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"));
-    table.put(put);
+    Configuration conf = HBaseConfiguration.create(); // co PutExample-1-CreateConf Create the configuration.
+
+    HTable table = new HTable(conf, "testtable"); // co PutExample-2-NewTable Instantiate a new client connection.
+
+    Put put = new Put(Bytes.toBytes("row1")); // co PutExample-3-NewPut Create put with specific row.
+
+    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
+      Bytes.toBytes("val1")); // co PutExample-4-AddCol Add a column to the put.
+
+    table.put(put); // co PutExample-5-DoPut Store row with column into HBase.
   }
 }
 // ^^ PutExample
