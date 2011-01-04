852919a6fc9cf46dc47ed6bdc4f7130e1d3addf5
Lars George <lars@larsgeorge.com>
2011-01-04 21:52:36 +0100

    Adds GetExample.

---
+++ ch04/src/main/java/GetExample.java
@@ -0,0 +1,31 @@
+// cc GetExample Example application inserting data into HBase
+// vv GetExample
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class GetExample {
+
+  public static void main(String[] args) throws IOException {
+    Configuration conf = HBaseConfiguration.create(); // co GetExample-1-CreateConf Create the configuration.
+
+    HTable table = new HTable(conf, "testtable"); // co GetExample-2-NewTable Instantiate a new client connection.
+
+    Get get = new Get(Bytes.toBytes("row1")); // co GetExample-3-NewGet Create get with specific row.
+
+    get.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1")); // co GetExample-4-AddCol Add a column to the get.
+
+    Result result = table.get(get); // co GetExample-5-DoGet Retrieve row with selected columns from HBase.
+
+    byte[] val = result.getValue(Bytes.toBytes("colfam1"),
+      Bytes.toBytes("qual1")); // co GetExample-6-GetValue Get a specific value for the given column.
+
+    System.out.println("Value: " + Bytes.toString(val)); // co GetExample-7-Print Print out the value while converting it back.
+  }
+}
+// ^^ GetExample
