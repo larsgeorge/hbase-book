40a140d6b83f9d734e1b35c81eb37a12d4ebd0f9
Lars George <lars@larsgeorge.com>
2011-04-15 12:20:28 +0200

    Suppresses excessive ZK logging. Checks args.

--- ch04/bin/run.sh
+++ ch04/bin/run.sh
@@ -3,27 +3,36 @@
 # run.sh: helps executing the examples by setting up the Java CLASSPATH.
 #
 
+# get the current directory
+bin=`dirname "$0"`
+bin=`cd "$bin">/dev/null; pwd`
+
+# check command line args
+if [[ $# == 0 ]]; then
+  echo "usage: $(basename $0) <example-name>"
+  exit 1;
+fi
+
+# set up Maven environment
 MVN="mvn"
 if [ "$MAVEN_HOME" != "" ]; then
   MVN=${MAVEN_HOME}/bin/mvn
 fi
 
-# CLASSPATH initially contains $HBASE_CONF_DIR
+# classpath initially contains $HBASE_CONF_DIR
 CLASSPATH="${HBASE_CONF_DIR}"
 
-# Need to generate classpath from maven pom. This is costly so generate it
-# and cache it. Save the file into our target dir so a mvn clean will get
-# clean it up and force us create a new one.
-cpfile="target/cached_classpath.txt"
-if [ ! -f "${cpfile}" ]
-then
-  ${MVN} -f "../pom.xml" dependency:build-classpath -Dmdep.outputFile="${cpfile}" &> /dev/null
+# add classes first, triggers log4j.properties priority
+if [ -d "${bin}/../target/classes" ]; then
+  CLASSPATH=${CLASSPATH}:${bin}/../target/classes
+fi
+
+# create and cache Maven classpath
+cpfile="${bin}/../target/cached_classpath.txt"
+if [ ! -f "${cpfile}" ]; then
+  ${MVN} -f "${bin}/../pom.xml" dependency:build-classpath -Dmdep.outputFile="${cpfile}" &> /dev/null
 fi
 CLASSPATH=${CLASSPATH}:`cat "${cpfile}"`
-
-if [ -d "target/classes" ]; then
-  CLASSPATH=${CLASSPATH}:target/classes
-fi
 
 JAVA=$JAVA_HOME/bin/java
 JAVA_HEAP_MAX=-Xmx512m
