e58341a14b5861c7094a21ed2b2274c1cba5510e
Lars George <lars@larsgeorge.com>
2011-04-26 22:16:35 -0700

    Adds template coprocessors.

---
+++ hush/src/main/java/com/hbasebook/hush/coprocessor/FetchUrlInfoObserver.java
@@ -0,0 +1,32 @@
+package com.hbasebook.hush.coprocessor;
+
+import org.apache.hadoop.hbase.coprocessor.BaseRegionObserverCoprocessor;
+import org.apache.hadoop.hbase.coprocessor.RegionObserver;
+
+import java.io.IOException;
+
+/**
+ * An observer implementation that checks for new URL records and
+ * asynchronously fetches some into about it.
+ */
+public class FetchUrlInfoObserver extends BaseRegionObserverCoprocessor
+  implements RegionObserver, FetchUrlInfoProtocol {
+
+
+  @Override
+  public void processQueue() {
+  }
+
+  @Override
+  public void updateUrl(String url) throws IOException {
+  }
+
+  @Override
+  public void updateUrlMD5(String urlMD5) throws IOException {
+  }
+
+  @Override
+  public long getProtocolVersion(String s, long l) throws IOException {
+    return 1L;
+  }
+}
---
+++ hush/src/main/java/com/hbasebook/hush/coprocessor/FetchUrlInfoProtocol.java
@@ -0,0 +1,16 @@
+package com.hbasebook.hush.coprocessor;
+
+import org.apache.hadoop.hbase.ipc.CoprocessorProtocol;
+
+import java.io.IOException;
+
+/**
+ * Defines a dynamic RPC call to initiate server side actions.
+ */
+public interface FetchUrlInfoProtocol extends CoprocessorProtocol {
+
+  void processQueue();
+
+  void updateUrl(String url) throws IOException;
+  void updateUrlMD5(String urlMD5) throws IOException;
+}
