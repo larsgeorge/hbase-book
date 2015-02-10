c574b1fc0660cf73466490c1f87276c72819b2ec
Lars George <lars@larsgeorge.com>
2015-02-10 09:58:31 +0100

    Copied start script improvements from previous work.
    This was in my local repo and adds class path caching etc.

---
+++ hush/bin/hush-config.sh
@@ -0,0 +1,75 @@
+# resolve links - "${BASH_SOURCE-$0}" may be a softlink
+this="${BASH_SOURCE-$0}"
+while [ -h "$this" ]; do
+  ls=`ls -ld "$this"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    this="$link"
+  else
+    this=`dirname "$this"`/"$link"
+  fi
+done
+
+# convert relative path to absolute path
+bin=`dirname "$this"`
+script=`basename "$this"`
+bin=`cd "$bin">/dev/null; pwd`
+this="$bin/$script"
+
+# the root of the installation
+if [ -z "$HUSH_HOME" ]; then
+  export HUSH_HOME=`dirname "$this"`/..
+fi
+
+#check to see if the conf dir or hush home are given as an optional arguments
+while [ $# -gt 1 ]
+do
+  if [ "--config" = "$1" ]
+  then
+    shift
+    confdir=$1
+    shift
+    HUSH_CONF_DIR=$confdir
+  else
+    break
+  fi
+done
+
+# Allow alternate hush conf dir location.
+HUSH_CONF_DIR="${HUSH_CONF_DIR:-$HUSH_HOME/conf}"
+
+# Source the hbase-env.sh.  Will have JAVA_HOME defined.
+if [ -f "${HUSH_CONF_DIR}/hush-env.sh" ]; then
+  . "${HUSH_CONF_DIR}/hush-env.sh"
+fi
+
+if [ -z "$JAVA_HOME" ]; then
+  for candidate in \
+    /usr/lib/jvm/java-6-sun \
+    /usr/lib/jvm/java-1.6.0-sun-1.6.0.* \
+    /usr/lib/j2sdk1.6-sun \
+    /usr/java/jdk1.6* \
+    /usr/java/jre1.6* \
+    /Library/Java/Home ; do
+    if [ -e $candidate/bin/java ]; then
+      export JAVA_HOME=$candidate
+      break
+    fi
+  done
+  # if we didn't set it
+  if [ -z "$JAVA_HOME" ]; then
+    cat 1>&2 <<EOF
++======================================================================+
+|      Error: JAVA_HOME is not set and Java could not be found         |
++----------------------------------------------------------------------+
+| Please download the latest Sun JDK from the Sun Java web site        |
+|       > http://java.sun.com/javase/downloads/ <                      |
+|                                                                      |
+| Hush requires Java 1.6 or later.                                    |
+| NOTE: This script will find Sun Java whether you install using the   |
+|       binary or the RPM based installer.                             |
++======================================================================+
+EOF
+    exit 1
+  fi
+fi
--- hush/bin/start-hush.sh
+++ hush/bin/start-hush.sh
@@ -7,33 +7,100 @@
 bin=`dirname "$0"`
 bin=`cd "$bin">/dev/null; pwd`
 
+# This will set HUSH_HOME, etc.
+. "$bin"/hush-config.sh
+
+# Detect if we are in hush sources dir
+in_dev_env=false
+if [ -d "${HUSH_HOME}/target" ]; then
+  in_dev_env=true
+  echo "Found development environment..."
+fi
+
 # set up Maven environment
 MVN="mvn"
 if [ "$MAVEN_HOME" != "" ]; then
   MVN=${MAVEN_HOME}/bin/mvn
 fi
 
-# classpath initially contains $HBASE_CONF_DIR
-CLASSPATH="${HBASE_CONF_DIR}"
-
-# add classes first, triggers log4j.properties priority
-if [ -d "${bin}/../target/classes" ]; then
-  CLASSPATH=${CLASSPATH}:${bin}/../target/classes
+# check envvars which might override default args
+if [ "$HUSH_HEAPSIZE" != "" ]; then
+  JAVA_HEAP_MAX="-Xmx${HUSH_HEAPSIZE}m"
 fi
 
-# create and cache Maven classpath
-cpfile="${bin}/../target/cached_classpath.txt"
-if [ ! -f "${cpfile}" ]; then
-  ${MVN} -f "${bin}/../pom.xml" dependency:build-classpath -Dmdep.outputFile="${cpfile}" &> /dev/null
-fi
-CLASSPATH=${CLASSPATH}:`cat "${cpfile}"`
+# classpath initially contains $HUSH_CONF_DIR
+CLASSPATH="${HUSH_CONF_DIR}"
 
-JAVA=$JAVA_HOME/bin/java
-JAVA_HEAP_MAX=-Xmx512m
+add_maven_main_classes_to_classpath() {
+  if [ -d "$HUSH_HOME/target/classes" ]; then
+    CLASSPATH=${CLASSPATH}:$HUSH_HOME/target/classes
+  fi
+}
+
+add_maven_test_classes_to_classpath() {
+  if [ -d "$HUSH_HOME/target/test-classes" ]; then
+    CLASSPATH=${CLASSPATH}:$HUSH_HOME/target/test-classes
+  fi
+}
+
+add_maven_deps_to_classpath() {
+  cpfile="${HUSH_HOME}/target/cached_classpath.txt"
+  echo "Adding libraries from cached file: $cpfile"
+  if [ ! -f "${cpfile}" ]; then
+    ${MVN} -f "${HUSH_HOME}/pom.xml" dependency:build-classpath -Dmdep.outputFile="${cpfile}" &> /dev/null
+  fi
+  CLASSPATH=${CLASSPATH}:`cat "${cpfile}"`
+}
+
+# Add maven target directory
+if $in_dev_env; then
+  # add classes first, triggers log4j.properties priority
+  add_maven_main_classes_to_classpath
+  add_maven_test_classes_to_classpath
+  # create and cache Maven classpath
+  add_maven_deps_to_classpath
+fi
+
+# For releases, add hush jar & webapps to CLASSPATH
+# Note: webapps must come first else it messes up Jetty
+if [ -d "$HUSH_HOME/webapp" ]; then
+  CLASSPATH=${CLASSPATH}:$HUSH_HOME
+fi
+for f in $HUSH_HOME/hush*.jar; do
+  if [ -f $f ]; then
+    CLASSPATH=${CLASSPATH}:$f;
+  fi
+done
+
+# Add libs to CLASSPATH
+for f in $HUSH_HOME/lib/*.jar; do
+  CLASSPATH=${CLASSPATH}:$f;
+done
+
+# default log directory & file
+if [ "$HUSH_LOG_DIR" = "" ]; then
+  HUSH_LOG_DIR="$HUSH_HOME/logs"
+fi
+if [ "$HUSH_LOGFILE" = "" ]; then
+  HUSH_LOGFILE='hush.log'
+fi
+
+HUSH_OPTS="$HUSH_OPTS -Dhush.log.dir=$HUSH_LOG_DIR"
+HUSH_OPTS="$HUSH_OPTS -Dhush.log.file=$HUSH_LOGFILE"
+HUSH_OPTS="$HUSH_OPTS -Dhush.home.dir=$HUSH_HOME"
+HUSH_OPTS="$HUSH_OPTS -Dhush.root.logger=${HUSH_ROOT_LOGGER:-INFO,console}"
+if [ "x$JAVA_LIBRARY_PATH" != "x" ]; then
+  HUSH_OPTS="$HUSH_OPTS -Djava.library.path=$JAVA_LIBRARY_PATH"
+fi
+
+JAVA="$JAVA_HOME/bin/java"
+JAVA_HEAP_MAX="-Xmx512m"
 
 echo "====================="
 echo " Starting Hush..."
 echo "====================="
 
+echo "Using classpath: $CLASSPATH"
+
 cd ${bin}/..
-"$JAVA" $JAVA_HEAP_MAX -classpath "$CLASSPATH" com.hbasebook.hush.HushMain
+"$JAVA" $JAVA_HEAP_MAX $HUSH_OPTS -classpath "$CLASSPATH" com.hbasebook.hush.HushMain
