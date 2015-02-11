f1a495a8eb642a5bce10e292969fe7b8591d32f9
Lars George <lars@larsgeorge.com>
2015-02-11 15:23:22 +0100

    Added as example for old API vs new API.
    Can be used in Appendix etc. to demonstrate the differences. Needs callout markup still, same with accompanying example for new API.

---
+++ ch03/src/main/java/client/CRUDExamplePreV1API.java
@@ -0,0 +1,54 @@
+package client;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+import util.HBaseHelper;
+
+import java.io.IOException;
+
+public class CRUDExamplePreV1API {
+
+  public static void main(String[] args) throws IOException {
+    Configuration conf = HBaseConfiguration.create();
+
+    HBaseHelper helper = HBaseHelper.getHelper(conf);
+    if (!helper.existsTable("testtable")) {
+      helper.createTable("testtable", "fam-A", "fam-B");
+    }
+
+    HTable table = new HTable(conf, "testtable");
+
+    Put put = new Put(Bytes.toBytes("myrow-1"));
+    put.add(Bytes.toBytes("fam-A"), Bytes.toBytes("col-A"),
+      Bytes.toBytes("val-1"));
+    put.add(Bytes.toBytes("fam-B"), Bytes.toBytes("col-B"),
+      Bytes.toBytes("val-2"));
+    table.put(put);
+
+    Get get = new Get(Bytes.toBytes("myrow-1"));
+    get.addColumn(Bytes.toBytes("fam-A"), Bytes.toBytes("col-A"));
+    Result result = table.get(get);
+    System.out.println(result);
+    byte[] val = result.getValue(Bytes.toBytes("fam-A"),
+      Bytes.toBytes("col-A"));
+    System.out.println("Value: " + Bytes.toString(val));
+
+    Delete delete = new Delete(Bytes.toBytes("myrow-1"));
+    delete.deleteColumns(Bytes.toBytes("fam-A"), Bytes.toBytes("col-A"));
+    table.delete(delete);
+
+    Scan scan = new Scan();
+    ResultScanner scanner = table.getScanner(scan);
+    for (Result result2 : scanner) {
+      System.out.println(result2);
+    }
+  }
+}
