e67692c8aa03c1e5abf7ecd4c95fea326dafe538
Lars George <lars@larsgeorge.com>
2015-02-12 16:40:17 +0100

    Updated helper in other modules.

--- ch04/src/main/java/util/HBaseHelper.java
+++ ch04/src/main/java/util/HBaseHelper.java
@@ -1,14 +1,17 @@
 package util;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
@@ -22,28 +25,53 @@ import java.util.Random;
 public class HBaseHelper {
 
   private Configuration conf = null;
-  private HBaseAdmin admin = null;
+  private Connection connection = null;
+  private Admin admin = null;
 
   protected HBaseHelper(Configuration conf) throws IOException {
     this.conf = conf;
-    this.admin = new HBaseAdmin(conf);
+    this.connection = ConnectionFactory.createConnection(conf);
+    this.admin = connection.getAdmin();
   }
 
   public static HBaseHelper getHelper(Configuration conf) throws IOException {
     return new HBaseHelper(conf);
   }
 
+  public void close() throws IOException {
+    connection.close();
+  }
+
+  public Connection getConnection() {
+    return connection;
+  }
+
   public boolean existsTable(String table)
+  throws IOException {
+    return existsTable(TableName.valueOf(table));
+  }
+
+  public boolean existsTable(TableName table)
   throws IOException {
     return admin.tableExists(table);
   }
 
   public void createTable(String table, String... colfams)
   throws IOException {
+    createTable(TableName.valueOf(table), null, colfams);
+  }
+
+  public void createTable(TableName table, String... colfams)
+  throws IOException {
     createTable(table, null, colfams);
   }
 
   public void createTable(String table, byte[][] splitKeys, String... colfams)
+  throws IOException {
+    createTable(TableName.valueOf(table), splitKeys, colfams);
+  }
+
+  public void createTable(TableName table, byte[][] splitKeys, String... colfams)
   throws IOException {
     HTableDescriptor desc = new HTableDescriptor(table);
     for (String cf : colfams) {
@@ -58,10 +86,18 @@ public class HBaseHelper {
   }
 
   public void disableTable(String table) throws IOException {
+    disableTable(TableName.valueOf(table));
+  }
+
+  public void disableTable(TableName table) throws IOException {
     admin.disableTable(table);
   }
 
   public void dropTable(String table) throws IOException {
+    dropTable(TableName.valueOf(table));
+  }
+
+  public void dropTable(TableName table) throws IOException {
     if (existsTable(table)) {
       disableTable(table);
       admin.deleteTable(table);
@@ -71,10 +107,23 @@ public class HBaseHelper {
   public void fillTable(String table, int startRow, int endRow, int numCols,
                         String... colfams)
   throws IOException {
+    fillTable(TableName.valueOf(table), startRow,endRow, numCols, colfams);
+  }
+
+  public void fillTable(TableName table, int startRow, int endRow, int numCols,
+                        String... colfams)
+  throws IOException {
     fillTable(table, startRow, endRow, numCols, -1, false, colfams);
   }
 
   public void fillTable(String table, int startRow, int endRow, int numCols,
+                        boolean setTimestamp, String... colfams)
+  throws IOException {
+    fillTable(TableName.valueOf(table), startRow, endRow, numCols, -1,
+      setTimestamp, colfams);
+  }
+
+  public void fillTable(TableName table, int startRow, int endRow, int numCols,
                         boolean setTimestamp, String... colfams)
   throws IOException {
     fillTable(table, startRow, endRow, numCols, -1, setTimestamp, colfams);
@@ -83,14 +132,30 @@ public class HBaseHelper {
   public void fillTable(String table, int startRow, int endRow, int numCols,
                         int pad, boolean setTimestamp, String... colfams)
   throws IOException {
-    fillTable(table, startRow, endRow, numCols, pad, setTimestamp, false, colfams);
+    fillTable(TableName.valueOf(table), startRow, endRow, numCols, pad,
+      setTimestamp, false, colfams);
+  }
+
+  public void fillTable(TableName table, int startRow, int endRow, int numCols,
+                        int pad, boolean setTimestamp, String... colfams)
+  throws IOException {
+    fillTable(table, startRow, endRow, numCols, pad, setTimestamp, false,
+      colfams);
   }
 
   public void fillTable(String table, int startRow, int endRow, int numCols,
                         int pad, boolean setTimestamp, boolean random,
                         String... colfams)
   throws IOException {
-    HTable tbl = new HTable(conf, table);
+    fillTable(TableName.valueOf(table), startRow, endRow, numCols, pad,
+      setTimestamp, random, colfams);
+  }
+
+  public void fillTable(TableName table, int startRow, int endRow, int numCols,
+                        int pad, boolean setTimestamp, boolean random,
+                        String... colfams)
+  throws IOException {
+    Table tbl = connection.getTable(table);
     Random rnd = new Random();
     for (int row = startRow; row <= endRow; row++) {
       for (int col = 0; col < numCols; col++) {
@@ -124,9 +189,28 @@ public class HBaseHelper {
     return res;
   }
 
+  public void put(String table, String row, String fam, String qual,
+                  String val) throws IOException {
+    put(TableName.valueOf(table), row, fam, qual, val);
+  }
+
+  public void put(TableName table, String row, String fam, String qual,
+                  String val) throws IOException {
+    Table tbl = connection.getTable(table);
+    Put put = new Put(Bytes.toBytes(row));
+    put.add(Bytes.toBytes(fam), Bytes.toBytes(qual), Bytes.toBytes(val));
+    tbl.put(put);
+    tbl.close();
+  }
+
   public void put(String table, String row, String fam, String qual, long ts,
                   String val) throws IOException {
-    HTable tbl = new HTable(conf, table);
+    put(TableName.valueOf(table), row, fam, qual, ts, val);
+  }
+
+  public void put(TableName table, String row, String fam, String qual, long ts,
+                  String val) throws IOException {
+    Table tbl = connection.getTable(table);
     Put put = new Put(Bytes.toBytes(row));
     put.add(Bytes.toBytes(fam), Bytes.toBytes(qual), ts,
             Bytes.toBytes(val));
@@ -136,7 +220,12 @@ public class HBaseHelper {
 
   public void put(String table, String[] rows, String[] fams, String[] quals,
                   long[] ts, String[] vals) throws IOException {
-    HTable tbl = new HTable(conf, table);
+    put(TableName.valueOf(table), rows, fams, quals, ts, vals);
+  }
+
+  public void put(TableName table, String[] rows, String[] fams, String[] quals,
+                  long[] ts, String[] vals) throws IOException {
+    Table tbl = connection.getTable(table);
     for (String row : rows) {
       Put put = new Put(Bytes.toBytes(row));
       for (String fam : fams) {
@@ -144,6 +233,7 @@ public class HBaseHelper {
         for (String qual : quals) {
           String val = vals[v < vals.length ? v : vals.length - 1];
           long t = ts[v < ts.length ? v : ts.length - 1];
+          System.out.println("Adding: " + fam + " " + qual + " " + t + " " + val);
           put.add(Bytes.toBytes(fam), Bytes.toBytes(qual), t,
             Bytes.toBytes(val));
           v++;
@@ -156,7 +246,12 @@ public class HBaseHelper {
 
   public void dump(String table, String[] rows, String[] fams, String[] quals)
   throws IOException {
-    HTable tbl = new HTable(conf, table);
+    dump(TableName.valueOf(table), rows, fams, quals);
+  }
+
+  public void dump(TableName table, String[] rows, String[] fams, String[] quals)
+  throws IOException {
+    Table tbl = connection.getTable(table);
     List<Get> gets = new ArrayList<Get>();
     for (String row : rows) {
       Get get = new Get(Bytes.toBytes(row));
@@ -172,10 +267,12 @@ public class HBaseHelper {
     }
     Result[] results = tbl.get(gets);
     for (Result result : results) {
-      for (KeyValue kv : result.raw()) {
-        System.out.println("KV: " + kv +
-          ", Value: " + Bytes.toString(kv.getValue()));
+      for (Cell cell : result.rawCells()) {
+        System.out.println("Cell: " + cell +
+          ", Value: " + Bytes.toString(cell.getValueArray(),
+          cell.getValueOffset(), cell.getValueLength()));
       }
     }
+    tbl.close();
   }
 }
