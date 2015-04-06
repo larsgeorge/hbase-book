9ce50e946dbf66f25a2c8d5d2b44bda15a0a1f3e
Lars George <lars@larsgeorge.com>
2015-04-06 19:05:49 +0200

    New example, ch04, copro, WIP.

---
+++ ch04/src/main/java/coprocessor/RegionServerObserverExample.java
@@ -0,0 +1,29 @@
+package coprocessor;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.coprocessor.BaseRegionServerObserver;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionServerCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.wal.WALKey;
+
+// cc RegionServerObserverExample Example region server observer that ...
+public class RegionServerObserverExample extends BaseRegionServerObserver {
+  public static final Log LOG = LogFactory.getLog(HRegion.class);
+
+  @Override
+  public void postMerge(ObserverContext<RegionServerCoprocessorEnvironment> c,
+    HRegion regionA, HRegion regionB, HRegion mergedRegion) throws IOException {
+    HRegionInfo regionInfo = mergedRegion.getRegionInfo();
+//    mergedRegion.getWAL().append(mergedRegion.getTableDesc(), regionInfo,
+//      new WALKey(mergedRegion.getRegionName(), regionInfo.getTable()),
+//      new WALEdit().add(CellUtil.createCell()))
+  }
+
+}
