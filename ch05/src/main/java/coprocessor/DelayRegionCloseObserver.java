fe39c15b9285a42ff7e095d7969983ad0ddc5e55
Lars George <lars@larsgeorge.com>
2015-04-12 19:40:47 +0200

    Observers to try delaying operations.

--- ch05/src/main/java/coprocessor/DelayRegionCloseObserver.java
+++ ch05/src/main/java/coprocessor/DelayRegionCloseObserver.java
@@ -1,4 +1,4 @@
-package admin;
+package coprocessor;
 
 import java.io.IOException;
 import java.util.Random;
@@ -6,10 +6,16 @@ import java.util.Random;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.io.FSDataInputStreamWrapper;
+import org.apache.hadoop.hbase.io.Reference;
+import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.regionserver.StoreFile;
 
 // cc DelayRegionCloseObserver Special test observer creating delays
 public class DelayRegionCloseObserver extends BaseRegionObserver {
@@ -19,17 +25,20 @@ public class DelayRegionCloseObserver extends BaseRegionObserver {
   private Random rnd = new Random();
 
   @Override
-  public void preClose(ObserverContext<RegionCoprocessorEnvironment> c,
-    boolean abortRequested) throws IOException {
+  public StoreFile.Reader preStoreFileReaderOpen(
+    ObserverContext<RegionCoprocessorEnvironment> ctx, FileSystem fs, Path p,
+    FSDataInputStreamWrapper in, long size, CacheConfig cacheConf, Reference r,
+    StoreFile.Reader reader) throws IOException {
     try {
       long delay = rnd.nextInt(3);
       LOG.info("@@@ Delaying region " +
-        c.getEnvironment().getRegion().getRegionNameAsString() +
+        ctx.getEnvironment().getRegion().getRegionNameAsString() +
         " for " + delay + " seconds...");
       Thread.sleep(delay * 1000);
     } catch (InterruptedException ie) {
       LOG.error(ie);
     }
+    return reader;
   }
   // ^^ DelayRegionCloseObserver
 }
---
+++ ch05/src/main/java/coprocessor/DelayingMasterObserver.java
@@ -0,0 +1,50 @@
+package coprocessor;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.CoprocessorEnvironment;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.ServerName;
+import org.apache.hadoop.hbase.coprocessor.BaseMasterObserver;
+import org.apache.hadoop.hbase.coprocessor.MasterCoprocessorEnvironment;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.master.AssignmentListener;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+
+// cc DelayingMasterObserver Special master observer that delays region asignments
+public class DelayingMasterObserver extends BaseMasterObserver
+  implements AssignmentListener {
+  public static final Log LOG = LogFactory.getLog(HRegion.class);
+
+  private Random rnd = new Random();
+
+  @Override
+  public void regionOpened(HRegionInfo hRegionInfo, ServerName serverName) {
+    try {
+      if (hRegionInfo.getTable().getQualifierAsString().equals("testtable")) {
+        long delay = rnd.nextInt(3);
+        LOG.info("@@@ Delaying region " +
+          hRegionInfo.getRegionNameAsString() +
+          " for " + delay + " seconds...");
+        Thread.sleep(delay * 1000);
+      }
+    } catch (InterruptedException ie) {
+      LOG.error(ie);
+    }
+  }
+
+  @Override
+  public void regionClosed(HRegionInfo hRegionInfo) {
+
+  }
+
+  @Override
+  public void start(CoprocessorEnvironment ctx) throws IOException {
+    MasterCoprocessorEnvironment env = (MasterCoprocessorEnvironment) ctx;
+    env.getMasterServices().getAssignmentManager().registerListener(this);
+  }
+}
+// ^^ DelayingMasterObserver
