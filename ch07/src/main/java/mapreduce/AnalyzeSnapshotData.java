f45dcb9c859e9adc6b2e0075c8689c9a5d8d4554
Lars George <lars@larsgeorge.com>
2015-06-30 15:51:52 +0200

    Fixed adddepjar call.

--- ch07/src/main/java/mapreduce/AnalyzeSnapshotData.java
+++ ch07/src/main/java/mapreduce/AnalyzeSnapshotData.java
@@ -237,7 +237,8 @@ public class AnalyzeSnapshotData {
     TableMapReduceUtil.initTableSnapshotMapperJob(snapshot, scan,
       AnalyzeMapper.class, Text.class, IntWritable.class, job, true,
       restoreDir); // co AnalyzeSnapshotData-2-Util Set up the snapshot mapper phase using the supplied utility.
-    TableMapReduceUtil.addDependencyJars(conf, JSONParser.class);
+    TableMapReduceUtil.addDependencyJars(job.getConfiguration(),
+      JSONParser.class);
     job.setReducerClass(AnalyzeReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
