f523b1fd66767cc04b1155b7c13ef0f2eff7fc90
Lars George <lars@larsgeorge.com>
2015-07-23 18:05:48 +0200

    Removed obsolete import.

--- ch08/src/main/java/client/ScanConsistencyExample1.java
+++ ch08/src/main/java/client/ScanConsistencyExample1.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Delete;
