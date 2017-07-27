1f7417e66a87a48971b2dc48190cf5b187a8902f
Lars George <lars@larsgeorge.com>
2017-07-27 22:58:04 +0200

    Updated ch13 ACL example

--- ch13/src/main/java/security/AccessControlExample.java
+++ ch13/src/main/java/security/AccessControlExample.java
@@ -9,11 +9,14 @@ import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
@@ -133,6 +136,36 @@ public class AccessControlExample {
         }
       });
     }
+
+    public void grant(final TableName tableName, final String user,
+      final Scan scan, final Permission.Action... action)
+      throws Exception {
+      doAs(new PrivilegedExceptionAction<Void>() {
+        @Override
+        public Void run() throws Exception {
+          Table table = connection.getTable(tableName);
+          ResultScanner scanner = table.getScanner(scan);
+          Map<String, Permission> perms = new HashMap<>();
+          perms.put(user, new Permission(action));
+          int rows = 0, cells = 0;
+          for (Result row : scanner) {
+            for (Cell cell : row.listCells()) {
+              Put put = new Put(cell.getRowArray(), cell.getRowOffset(),
+                cell.getRowLength());
+              put.add(cell);
+              put.setACL(perms);
+              table.put(put); // put per cell to avoid possible OOMEs
+              System.out.println("Put: " + put);
+              cells++;
+            }
+            rows++;
+          }
+          System.out.println("Processed " + rows + " rows and " +
+            cells + " cells.");
+          return null;
+        }
+      });
+    }
     /*...*/
     // ^^ AccessControlExample
 
@@ -233,10 +266,14 @@ public class AccessControlExample {
           try {
             List<UserPermission> ups = ups = AccessControlClient.
               getUserPermissions(connection, tableRegex);
-            System.out.println("User permissions:");
+            System.out.println("User permissions (" +
+              (tableRegex != null ? tableRegex : "hbase:acl") + ":");
+            int count = 0;
             for (UserPermission perm : ups) {
               System.out.println("  " + perm);
+              count++;
             }
+            System.out.println("Found " + count + " permissions.");
           } catch (Throwable throwable) {
             throw new RuntimeException(throwable);
           }
@@ -255,12 +292,48 @@ public class AccessControlExample {
             System.out.println("Starting scan...");
             int rows = 0;
             for (Result result: resultScanner) {
-              System.out.println(result);
+              System.out.println("  " + result);
               rows++;
             }
             System.out.println("Found " + rows + " rows.");
+            resultScanner.close();
+            table.close();
           } catch (Exception e) {
-            System.out.println("Scan failed with: " + e);
+            System.out.println("Scan failed with: " + e.getMessage());
+          }
+          return null;
+        }
+      });
+    }
+
+    public void put(final TableName tableName, final Put put)
+      throws Exception {
+      doAs(new PrivilegedExceptionAction<Void>() {
+        @Override
+        public Void run() throws Exception {
+          Table table = connection.getTable(tableName);
+          try {
+            table.put(put);
+          } catch(Exception e) {
+            System.out.println("Put failed with: " + e);
+          }
+          return null;
+        }
+      });
+    }
+
+    public Result get(final TableName tableName, final Get get)
+      throws Exception {
+      return doAs(new PrivilegedExceptionAction<Result>() {
+        @Override
+        public Result run() throws Exception {
+          Table table = connection.getTable(tableName);
+          try {
+            Result result = table.get(get);
+            System.out.println("Get result: " + result);
+            return result;
+          } catch(Exception e) {
+            System.out.println("Get failed with: " + e);
           }
           return null;
         }
@@ -352,7 +425,7 @@ public class AccessControlExample {
 
         List<UserPermission> ups = null;
         try {
-          ups = AccessControlClient.getUserPermissions(connection, ".*"); // co AccessControlExample-09-PrintPerms Print the current permissions.
+          ups = AccessControlClient.getUserPermissions(connection, ".*"); // co AccessControlExample-09-PrintPerms Print the current permissions for all tables.
           System.out.println("User permissions:");
           for (UserPermission perm : ups) {
             System.out.println("  " + perm);
@@ -368,26 +441,77 @@ public class AccessControlExample {
     // ^^ AccessControlExample
     System.out.println("Superuser: Grant global admin to hbasebook...");
     // vv AccessControlExample
-    superuser.grant(admin.getShortUserName(), Permission.Action.ADMIN);
+    superuser.grant(admin.getShortUserName(), Permission.Action.values());
 
     // ^^ AccessControlExample
     System.out.println("Admin & App1: Print permissions...");
     // vv AccessControlExample
     admin.printUserPermissions(null);
+    admin.printUserPermissions(".*");
     app1.printUserPermissions(tableName.toString());
-    // ^^ AccessControlExample
-    System.out.println("Application: Attempting to scan table...");
-    // vv AccessControlExample
-    app1.scan(tableName, new Scan());
 
     // ^^ AccessControlExample
-    System.out.println("Admin: Grant table access to application...");
+    System.out.println("Application: Attempting to scan table, should fail...");
+    // vv AccessControlExample
+    app1.scan(tableName, new Scan()); // co AccessControlExample-10-ScanFail The scan will fail with an access denied message because the application user has no access permissions granted.
+    // ^^ AccessControlExample
+    System.out.println("Admin: Grant table read access to application...");
     // vv AccessControlExample
     admin.grant(tableName, app1.getShortUserName(), "colfam1", "col-1",
       Permission.Action.READ);
     app1.printUserPermissions(tableName.toString());
     // ^^ AccessControlExample
     System.out.println("Application: Attempting to scan table again...");
+    // vv AccessControlExample
+    app1.scan(tableName, new Scan()); // co AccessControlExample-11-ScanSuccessThe second scan will work and only return one column from the otherwise unrestricted scan.
+
+    // ^^ AccessControlExample
+    System.out.println("Admin: Grant table write access to application...");
+    // vv AccessControlExample
+    admin.grant(tableName, app1.getShortUserName(), "colfam1", "col-acl", // co AccessControlExample-12-ColQual Grant write access to the application for a single new column (which does not exist yet).
+      Permission.Action.WRITE);
+    // ^^ AccessControlExample
+    System.out.println("Application: Write into table...");
+    // vv AccessControlExample
+    Put put = new Put(Bytes.toBytes("row-1"));
+    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("col-acl"),
+      Bytes.toBytes("val-acl"));
+    app1.put(tableName, put); // co AccessControlExample-13-WriteColumn Insert a value into the granted column.
+    // ^^ AccessControlExample
+    System.out.println("Application: Scanning table, value not visible...");
+    // vv AccessControlExample
+    app1.scan(tableName, new Scan(Bytes.toBytes("row-1"), // co AccessControlExample-14-ScanColumn Scanning the table does not show the write-only column, and a direct read of the column fails with an access denied error.
+      Bytes.toBytes("row-10")));
+    // ^^ AccessControlExample
+    System.out.println("Application: Attempting to directly access column, will fail...");
+    // vv AccessControlExample
+    Get get = new Get(Bytes.toBytes("row-1"));
+    get.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("col-acl"));
+    app1.get(tableName, get);
+
+    // ^^ AccessControlExample
+    System.out.println("Admin: Grant read to application for new column...");
+    // vv AccessControlExample
+    Scan scan = new Scan(Bytes.toBytes("row-1"),
+      Bytes.toBytes("row-10"));
+    scan.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("col-acl"));
+    admin.grant(tableName, app1.getShortUserName(), scan,
+      Permission.Action.READ);
+    // ^^ AccessControlExample
+    System.out.println("Application: Read new column...");
+    // vv AccessControlExample
+    app1.scan(tableName, new Scan(Bytes.toBytes("row-1"),
+      Bytes.toBytes("row-10")));
+
+    // ^^ AccessControlExample
+    System.out.println("Admin: Revoking all access for application...");
+    // vv AccessControlExample
+    admin.revoke(tableName, app1.getShortUserName(), "colfam1", "col-1",
+      Permission.Action.values());
+    admin.revoke(tableName, app1.getShortUserName(), "colfam1", "col-acl",
+      Permission.Action.values());
+    // ^^ AccessControlExample
+    System.out.println("Application: Attempting to scan, should fail...");
     // vv AccessControlExample
     app1.scan(tableName, new Scan());
   }
